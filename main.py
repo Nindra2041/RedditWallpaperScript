48d84afb3399a30ced67ec610b46e351af1f3f7d
Nindra2041 <nindra2400@gmail.com>
2020-10-30 14:28:49 +1100

    wallpaper script that pulls images off reddit

---
+++ main.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+from pynput.keyboard import Key, Listener, KeyCode
+import os
+import praw
+import tkinter
+from tkinter import messagebox as msg
+#TODO: figure out how to get image from gallery
+#TODO: make it run when linux starts
+reddit = praw.Reddit(
+     client_id="your client id",
+     client_secret="your client secret",
+     user_agent="script desc"
+)
+state = True
+def on_press(key):
+    global state
+    #check if wallpaper can be changed
+    if state:
+        rnd = reddit.subreddit('earthporn').random()
+        #set to random wallpaper off reddit
+        if key == Key.alt_r:
+            is_pic = False
+            while not is_pic:
+                if rnd.url[-4:] == ".jpg" or rnd.url[-4] == ".png" or rnd.url[-5] == ".jpeg":
+                    is_pic = True
+                    os.system("/usr/bin/gsettings set org.gnome.desktop.background picture-uri " + rnd.url)
+                    #uncomment this line if you want to view the whole image
+                    #os.system("gsettings set org.gnome.desktop.background picture-options \"scaled\"")
+                else:
+                     rnd = reddit.subreddit('Overwatch_Porn').random()
+        if key == KeyCode.from_char('/'):
+            root = tkinter.Tk()
+            root.withdraw()
+            msg.showinfo("Wallpaper Help", "Commands:\nRight Alt: Change Wallpaper\nHome Key: Toggle the ability to change wallpaper\nF12: Reset wallpaper\nEsc: Reset wallpaper and exit")
+    #toggle state
+    if key == Key.home:
+        if state:
+            state = False
+        else:
+            state = True
+    #set wallpaper to default
+    if key == Key.f12:
+        os.system("/usr/bin/gsettings set org.gnome.desktop.background picture-uri /usr/share/backgrounds/warty-final-ubuntu.png")
+        os.system("gsettings set org.gnome.desktop.background picture-options \"zoom\"")
+    #exit program and change to default
+    if key == Key.esc:
+        os.system("/usr/bin/gsettings set org.gnome.desktop.background picture-uri /usr/share/backgrounds/warty-final-ubuntu.png")
+        os.system("gsettings set org.gnome.desktop.background picture-options \"zoom\"")
+        exit(0)
+
+with Listener(on_press=on_press) as listen:
+    listen.join()
